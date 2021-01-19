37bf59499e239cc1d4d84b17a99a0adf8fda3749
hiroghap1 <hiro_ghap1@yahoo.co.jp>
2021-01-19 12:54:33 +0900

    四捨五入

--- js/script.js
+++ js/script.js
@@ -1,28 +1,19 @@
 const calcBtn =document.getElementById('calc-btn');
+const base = 100;// 四捨五入桁数
 
 calcBtn.addEventListener('click',()=>{
    const apparent = document.getElementById('apparent').value;
    const eyepiece = document.getElementById('eyepiece').value;
    const objectiveLens = document.getElementById('objective-lens').value;
 
-   console.log(apparent,eyepiece,objectiveLens);
-
    const tan = Math.tan(apparent / 2 * Math.PI / 180);
    const tan2 = tan * eyepiece / objectiveLens;
     const atan = 2 * Math.atan(tan2) / Math.PI * 180;
    const calc = apparent / atan;
-    console.log(Math.atan(1));
-    console.log(apparent);
-    console.log(tan);
-    console.log(tan2);
-    console.log(atan);
-    console.log(calc);
 
     const magnification =document.getElementById('magnification');
-    magnification.innerText = calc;
+    magnification.innerText = Math.round(calc * base) / base;
     const result = document.getElementById('result');
     result.style.display = 'block';
 
 });
-
-//40/(2*atan(tan(40/2)*20/1000))
